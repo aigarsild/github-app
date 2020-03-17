e37378ea1f2ac7ea1bbb1a7134f632b277072994
Aigar Sild <aigar.sild@vaimo.com>
2020-03-17 19:45:39 +0200

    Added font awesome icons, added vuex, added data checks for arrays, added favorites styles

--- src/bundles/search/searchComponent.vue
+++ src/bundles/search/searchComponent.vue
@@ -5,7 +5,7 @@
       <h1 class="display-3 text-center w-100">Search for a Github repo.</h1>
       <p class="lead text-center w-100">Search results are fetched trough Github API</p>
       <section class="search">
-        <input class="searchbar rounded shadow border-0 p-3 w-100" v-on:keyup="getCall" type="text">
+        <input class="searchbar rounded shadow border-0 p-3 w-100" v-on:keyup="getSearchResults" type="text">
         <div class="spinner-border" role="status" v-if="loading">
           <span class="sr-only">Loading...</span>
         </div>
@@ -16,15 +16,18 @@
         <div v-for="result in searchResults" :key="result.id" class="col-3 mt-4">
           <div class="card">
             <div class="card-body">
+
               <h5 class="card-title">{{ result.name }}</h5>
               <p class="card-text">Author: <strong>{{ result.owner.login }}</strong></p>
               <p class="card-text">Stars: <strong>{{ result.stargazers_count }}</strong></p>
               <p class="card-text">Forks: <strong>{{ result.forks }}</strong></p>
               <a target="_blank" :href="result.html_url" class="btn btn-primary w-100 ">Go to repo page</a>
-              <p class="btn btn-dark mt-3 w-100 " ref="addToBookmarkBtn" v-on:click="addToBookmark" :data-bookmark-id="result.id">Add to bookmark</p>
-              <div class="alert alert-info" role="alert">
-                Bookmark already saved!
-              </div>
+
+              <p class="btn btn-dark mt-3 w-100" v-on:click="bookmarkBtnAction(result.bookmarkStatus, result.id)" :data-bookmark-id="result.id">
+                <span v-if="!result.bookmarkStatus">Add bookmark <font-awesome-icon class="bookmarks-star-empty" icon="star" /></span>
+                <span v-if="result.bookmarkStatus">Remove bookmark <font-awesome-icon class="bookmarks-star" icon="star" /></span>
+              </p>
+
             </div>
           </div>
         </div>
@@ -39,10 +42,8 @@
   import axios from 'axios';
   import {_} from 'vue-underscore';
 
-
   export default {
     name: 'Search',
-
     props: {
       msg: String
     },
@@ -52,35 +53,45 @@
         loading: false,
         searchValue: String,
         searchResults: [],
-        requestUrl: String,
-        noSearchResults: false
+        requestUrl: 'https://api.github.com/search/repositories?sort=stars&q=',
+        noSearchResults: false,
+        checkForId: [],
+        localStorageKey: 'bookmarkIds'
       }
     },
 
     methods: {
-      getCall: _.debounce(function (e) {
-        this.searchValue = e.target.value;
-        this.loading = true;
-        this.requestUrl = 'https://api.github.com/search/repositories?sort=stars&q=' + this.searchValue;
+      getSearchResults: function(searchValue) {
+        this.searchValue = searchValue.target.value;
+        this.setParams();
+      },
 
-        if (!this.searchValue) {
-          this.loading = false;
-          this.searchResults = [];
+      setParams: function() {
+        if (this.searchValue) {
+          this.getCall();
           return;
         }
 
-        axios.get(this.requestUrl)
+        this.loading = false;
+        this.searchResults = [];
+      },
+
+      getCall: _.debounce(function () {
+        this.loading = true;
+
+        axios.get(this.requestUrl + this.searchValue)
                 .then((response)  =>  {
                   this.loading = false;
 
-                  if (!response.data.total_count) {
-                    this.noSearchResults = true;
-                    this.searchResults = [];
+                  if (response.data.total_count) {
+                    this.noSearchResults = false;
+                    this.searchResults = this.checkForBookmarks(response.data.items);
+
                     return;
                   }
 
-                  this.noSearchResults = false;
-                  this.searchResults = response.data.items;
+                  this.noSearchResults = true;
+                  this.searchResults = [];
 
                 }).catch(error => {
                   console.error(error);
@@ -88,40 +99,57 @@
         });
       }, 500),
 
-      addToBookmark: function (e) {
-        if (localStorage.getItem('bookmarkIds')) {
-          try {
-            const retrievedData = JSON.parse(localStorage.getItem('bookmarkIds'));
-            const elementId = e.target.dataset.bookmarkId;
+      checkForBookmarks: function(searchResults) {
+        const retrievedBookmarkData = JSON.parse(localStorage.getItem(this.localStorageKey));
+        //Parse bookmark ids to int
+        const parsedBookmarkData = retrievedBookmarkData.map(id => parseInt(id, 10));
 
-            //Check if id is already stored
-            const checkForId = retrievedData.filter(id => elementId !== id ? true : this.alreadyAdded());
-            this.pushItemToBookmark(checkForId, elementId);
-          } catch(e) {
-            localStorage.removeItem('bookmarkIds');
-          }
+        //If fetched search results contain bookmarked id then add new property bookmarkStatus to results
+        searchResults.filter(element => (parsedBookmarkData.includes(element.id) ? element.bookmarkStatus = true : element.bookmarkStatus = false));
+        return searchResults;
+      },
+
+      bookmarkBtnAction: function(bookmarkStatus, bookmarkId) {
+
+        if (bookmarkStatus) {
+          this.removeBookmark(bookmarkId);
+         return;
+        }
+
+        this.addToBookmark(bookmarkId);
+      },
+
+      addToBookmark: function(elementId) {
+        if (localStorage.getItem(this.localStorageKey)) {
+          const retrievedData = JSON.parse(localStorage.getItem(this.localStorageKey));
+
+          //Check if id is already stored
+          this.checkForId = retrievedData.filter(id => elementId !== id ? true : this.itemAlreadyAddedToBookmarks());
+          this.pushItemToBookmark(this.checkForId, elementId);
 
           return;
         }
 
-        let bookmarks = [];
-        const bookmarkId = e.target.dataset.bookmarkId;
-
-        this.pushItemToBookmark(bookmarks, bookmarkId);
+        this.pushItemToBookmark(this.checkForId, elementId);
       },
 
-      pushItemToBookmark: function (array, id) {
+      removeBookmark: function(bookmarkId) {
+        console.log('removeeee' + bookmarkId);
+      },
+
+      pushItemToBookmark: function(array, id) {
         if (!id) {
           return;
         }
 
         array.push(id);
-        localStorage.setItem('bookmarkIds', JSON.stringify(array));
+        localStorage.setItem(this.localStorageKey, JSON.stringify(array));
       },
 
-      alreadyAdded: function () {
+      itemAlreadyAddedToBookmarks: function() {
         return false;
       }
+
     },
   }
 </script>
@@ -145,5 +173,20 @@
 
     .searchbar:focus, .searchbar:focus {
     outline: none;
-  }
+    }
+
+    .add-to-bookmarks, .remove-from-bookmarks {
+      color: gold;
+    }
+
+    .bookmarks-star path {
+      fill: gold;
+    }
+
+    .bookmarks-star-empty path {
+      stroke: gold;
+      fill: none;
+      stroke-width: 60px;
+    }
+
 </style>
--- src/main.js
+++ src/main.js
@@ -2,10 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import 'bootstrap/dist/css/bootstrap.css';
 import underscore from 'vue-underscore';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faStar, faStarHalf } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 Vue.config.productionTip = false;
 Vue.use(underscore);
 
+library.add(faStar, faStarHalf);
+Vue.component('font-awesome-icon', FontAwesomeIcon);
+
 new Vue({
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app');
